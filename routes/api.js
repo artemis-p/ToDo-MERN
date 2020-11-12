994ffb48c6bc7974e5dd467236963a0301b3e5bc
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-11-12 14:16:52 +0000

    Created routes

---
+++ routes/api.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/todos', (req, res, next) => {
+
+});
+
+router.post('/todos', (req, res, next) => {
+
+});
+
+router.delete('/todos', (req, res, next) => {
+
+});
+
+module.exports = router;
